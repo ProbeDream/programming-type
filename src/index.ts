3180b99b3187688e8813fabb11c2a6cb47bc4954
ProbeDream <gump1016@163.com>
2020-11-14 23:14:08 +0800

    添加代码示例!

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,10 @@ let c = {
     banana: b
 }
 let d = c.apple * 4;
+
+const sum = (a: number, b: number): number => a + b
+console.log(sum(2, 3));
+
+let e: any = 666;
+let f: any = ["danger"];
+console.log(e + f);
